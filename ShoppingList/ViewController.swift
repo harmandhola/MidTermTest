39e31b9294a912b09a941abc6bed53b2f6c96777
Ramandeep <rmnkaul979697@gmail.com>
2019-10-16 19:18:48 -0400

    Stepper Control is working now.

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //label
+    @IBOutlet weak var Quantity: UILabel!
+    
+    @IBAction func QuantityControl(_ sender: UIStepper) {
+        Quantity.text = String(sender.value)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
